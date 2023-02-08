12691a6af02e35df5db1a7733426683341c82f5e
Koushik Dey <koushik.dey@live.com>
2023-02-09 00:48:22 +0200

    creating button component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import { IoAirplaneSharp, IoAtCircleSharp, IoCaretForwardCircleSharp } from "react-icons/io5";
 import Button from './Button';
 
 const App = () => {
+  const handleClick = () => {
+    console.log('Button clicked')
+  };
+
   return (
     <div>
       <div>
-        <Button success rounded outline>Click</Button>
+        <Button primary outline rounded onClick={handleClick}>
+          <IoAirplaneSharp />
+          Continue
+        </Button>
       </div>
       <div>
-        <Button danger outline>Buy now!</Button>
+        <Button secondary outline rounded onClick={handleClick}>
+          <IoAtCircleSharp />
+          Buy now!
+        </Button>
       </div>
       <div>
-        <Button>Deal</Button>
+        <Button success outline rounded onClick={handleClick}>
+          <IoCaretForwardCircleSharp />
+          Click here
+        </Button>
+      </div>
+      <div>
+        <Button warning outline rounded onClick={handleClick}>
+          Warning
+        </Button>
+      </div>
+      <div>
+        <Button danger outline rounded onClick={handleClick}>
+          Danger
+        </Button>
       </div>
     </div>
   )
