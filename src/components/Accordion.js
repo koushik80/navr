df7e3b676c8dca9adec8e0eafa7009bd900abeeb
Koushik Dey <koushik.dey@live.com>
2023-02-09 21:13:02 +0200

    rendered items

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,33 @@
-
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+    };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    //const content = isExpanded && <div>{item.content}</div>;
+    const icon = <span className="text-rose-400">
+      {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+    </span>
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div className="flex p-3 bg-gray-50 border-b-2 items-center cursor-pointer" onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
   return (
-    <div>{renderedItems}</div>
+    <div className="border-x border-t rounded">{renderedItems}</div>
   )
 }
 
