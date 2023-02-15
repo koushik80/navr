314f483d62999a14bafd7d7f52028817645702a9
Koushik Dey <koushik.dey@live.com>
2023-02-15 04:32:51 +0200

    refactored the table component reusable

--- README.md
+++ README.md
@@ -7,5 +7,9 @@
 </div>
 
 <div>
-<h3 align="left">This is a practice app</h3>
+<h3 align="left">This is a practice app from sketch</h3>
+</div>
+
+<div>
+<p align="left">Creating this dummy app to use its components and code architecture for real-life applications in future.</p>
 </div>
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -4,13 +4,32 @@ const TablePage = () => {
   const data = [
     { name: 'Orange', color: 'bg-orange-500', score: 5 },
     { name: 'Apple', color: 'bg-red-600', score: 3 },
-    { name: 'Banana', color: 'bg-yellow-400', score: 1 },
+    { name: 'Banana', color: 'bg-yellow-400', score: 6 },
     { name: 'Lime', color: 'bg-green-400', score: 4 }
   ];
 
+  const config = [
+    {
+      label: 'Name',
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: 'Color',
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: 'Score',
+      render: (fruit) => fruit.score,
+    }
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
